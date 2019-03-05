783d2eeb1aa1bc775573a5e2ed165ed9d830fe2f
atrasmo <alessandro.trasmondi@gmail.com>
2019-03-05 20:34:46 +0100

    update file_management

--- domatrasi.py
+++ domatrasi.py
@@ -4,6 +4,64 @@ import requests
 import hashlib
 import sys
 import apikey
+import re
+
+def doitbydomain(connection_details,dominio):
+    ''' function that give you the auth_info passing domain_name '''
+
+    xml = '''
+    <?xml version='1.0' encoding='UTF-8' standalone='no' ?>
+    <!DOCTYPE OPS_envelope SYSTEM 'ops.dtd'>
+    <OPS_envelope>
+    <header>
+        <version>0.9</version>
+    </header>
+    <body>
+    <data_block>
+        <dt_assoc>
+            <item key="protocol">XCP</item>
+            <item key="action">GET</item>
+            <item key="object">DOMAIN</item>
+            <item key="attributes">
+                <dt_assoc>
+                    <item key="domain">'''+dominio+'''</item>
+                    <item key="type">domain_auth_info</item>
+                </dt_assoc>
+            </item>
+        </dt_assoc>
+    </data_block>
+    </body>
+    </OPS_envelope>
+    '''
+
+    md5_obj = hashlib.md5()
+    md5_obj.update((xml + connection_details['api_key']).encode('utf-8'))
+    signature = md5_obj.hexdigest()
+
+    md5_obj = hashlib.md5()
+    md5_obj.update((signature + connection_details['api_key']).encode('utf-8'))
+    signature = md5_obj.hexdigest()
+
+    headers = {
+            'Content-Type':'text/xml',
+            'X-Username': connection_details['reseller_username'],
+            'X-Signature':signature,
+    }
+
+    #debug points
+    #print ('***********************************',xml,'***********************************')
+    r = requests.post(connection_details['api_host_port'], data = xml, headers=headers )
+
+    if r.status_code == requests.codes.ok:
+        #print(r.text)
+        return (re.search(r"auth_info\">(.*)<",r.text)).group(1)
+    else:
+        return (r.status_code)
+        #_filewr.write(r.status_code)
+        #print (r.text)
+        #_filewr.write(r.text)
+
+
 parser = argparse.ArgumentParser()
 group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument("-d", metavar='<domain name>', help="domain name to check <e.g. btitalia.it> ", type=str )
@@ -11,6 +69,7 @@ group.add_argument("-f", metavar='<file name>', help="text file containing domai
 parser.add_argument("mode", help="0 or 1 - 0 for real operation and 1 just for test  ", type=int )
 parser.add_argument("-v", "--verbosity", help="output operation on screen", action = "store_true")
 args = parser.parse_args()
+#debug points
 #print(args)
 #print(apikey.api_key)
 
@@ -31,9 +90,9 @@ connection_options = {
              'reseller_username': apikey.username,
              'api_key': apikey.api_key,
              'api_host_port': 'https://horizon.opensrs.net:55443',
-
-        }
+            }
 }
+
 
 if TEST_MODE == 1:
     connection_details = connection_options['test']
@@ -41,55 +100,17 @@ else:
     connection_details = connection_options['live']
 
 
-xml = '''
-<?xml version='1.0' encoding='UTF-8' standalone='no' ?>
-<!DOCTYPE OPS_envelope SYSTEM 'ops.dtd'>
-<OPS_envelope>
-<header>
-    <version>0.9</version>
-</header>
-<body>
-<data_block>
-    <dt_assoc>
-        <item key="protocol">XCP</item>
-        <item key="action">GET</item>
-        <item key="object">DOMAIN</item>
-        <item key="attributes">
-            <dt_assoc>
-                <item key="domain">'''+dominio+'''</item>
-                <item key="type">domain_auth_info</item>
-            </dt_assoc>
-        </item>
-    </dt_assoc>
-</data_block>
-</body>
-</OPS_envelope>
-'''
-
-md5_obj = hashlib.md5()
-md5_obj.update((xml + connection_details['api_key']).encode('utf-8'))
-signature = md5_obj.hexdigest()
-
-md5_obj = hashlib.md5()
-md5_obj.update((signature + connection_details['api_key']).encode('utf-8'))
-signature = md5_obj.hexdigest()
-
-headers = {
-        'Content-Type':'text/xml',
-        'X-Username': connection_details['reseller_username'],
-        'X-Signature':signature,
-};
-print ('****************************')
-print (headers)
-print ('****************************')
-print (xml)
-print ('****************************')
-r = requests.post(connection_details['api_host_port'], data = xml, headers=headers )
-if r.status_code == requests.codes.ok:
-    print(r.text)
-    #_filewr.write(r.text)
+if (args.f != None):
+    try:
+        with open(args.f) as filein :
+            print (filein)
+            for dominio in filein:
+                dominio=  dominio.rstrip("\n")
+                auth_code = doitbydomain(connection_details, dominio)
+                print(dominio , "|",auth_code)
+            filein.close()
+    except IOError as e:
+        print ("Unable to open file , Does not exist OR no read permissions")
 else:
-    print (r.status_code)
-    #_filewr.write(r.status_code)
-    print (r.text)
-    #_filewr.write(r.text)
+    auth_code = doitbydomain(connection_details, dominio)
+    print(dominio , "|",auth_code)
