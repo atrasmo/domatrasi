0899b53e6e78392bc8fbb20fd64938502be0ef7c
Alessandro <atrasmo@users.noreply.github.com>
2019-03-05 16:08:29 +0100

    Update apikey.py

--- apikey.py
+++ apikey.py
@@ -1,2 +1,2 @@
-username = "btitaliaspa"
-api_key = "4b9f6b4d7e1e0969715e1cc07ddca738929570429042a22b68ecafdda2f7f5ac73a9cab2b93e9c660ee640104da8ee5342151feb44dd2d49"
+username = "your_user_name"
+api_key = "your_api_key"
